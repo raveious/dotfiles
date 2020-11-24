6cb0d365cbe5d15281c950ac901e623c93d36c80
Ian Wakely <raveious.irw@gmail.com>
2020-11-24 10:03:29 -0500

    Adding some more help to the installer scripts.

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+cd $(dirname $0)
+
+repo_dir="$(git rev-parse --show-toplevel)"
+
 echo "Installing dotfiles..."
 
 for dir in *; do
   if [ -f $dir/install.sh ]; then
     echo "Installing $dir configs..."
-    $dir/install.sh
+    . $dir/install.sh
   fi
 done
